274e66dda9cf3d6c303450199372917be29a5f96
yelucie <lucie.ye@reseau.eseo.fr>
2023-10-18 19:32:25 -0500

    JS part

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+// Welcome user
+const userConfirmed = confirm('Welcome on Lucie\'s Midterm Project. \nAre you ready to continue?') ?
+    alert('Great! Let\'s dive into this.') : alert('Goodbye then... Actually, we\'ll still check this out!');
+
+document.addEventListener("DOMContentLoaded", (evt) => {
     console.log("DOM fully loaded and parsed");
+
+    // Dark Mode for the page
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    
+    darkModeToggle.addEventListener("change", () =>
+        darkModeToggle.checked ? document.body.classList.add("dark-mode") : document.body.classList.remove("dark-mode")
+    );
 
     // Color-Picker in HTML section
     const colorPicker = document.getElementById('color-picker');
     const coloredBox = document.querySelector('#html .bonus');
 
-    colorPicker.addEventListener('input', function () {
-        coloredBox.style.backgroundColor = colorPicker.value;
-    });
+    colorPicker.addEventListener('input', () =>
+        coloredBox.style.backgroundColor = colorPicker.value
+    );
+
 });
